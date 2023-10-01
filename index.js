56881207c96d81cca962aea9c622abc2292fda60
adityaaa009 <114949288+adityaaa009@users.noreply.github.com>
2023-10-02 01:02:31 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+// const loginForm = document.getElementById('loginForm');
+
+// loginForm.addEventListener('submit', function (event) {
+//     const username = document.getElementById('username').value;
+//     const password = document.getElementById('password').value;
+
+//     // You can add your own validation logic here, e.g., checking against a database.
+//     if (username === 'your_username' && password === 'your_password') {
+//         // Successful login, allow the form to submit
+//         return;
+//     } else {
+//         // Prevent the form from submitting
+//         event.preventDefault();
+//         alert('Invalid username or password. Please try again.');
+//     }
+// });
